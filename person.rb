694c755409164f148d9dec3e79944ed18beaa5ba
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-08 18:54:43 +0530

    Update person rb file

--- person.rb
+++ person.rb
@@ -1,6 +1,6 @@
-require './person'
+require './nameable'
 
-class Person
+class Person < Nameable
   def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
@@ -10,6 +10,10 @@ class Person
 
   attr_accessor :name, :age
   attr_reader :id
+
+  def correct_name
+    @name
+  end
 
   def can_use_services?
     of_age? == true || parent_permission == true
