03c011d792de15b747988efd90280332868597cb
gauravgangwarweb <gauravgangwar501@gmail.com>
2023-03-10 21:55:15 +0530

    Add required changes

--- person.rb
+++ person.rb
@@ -1,11 +1,12 @@
 require './nameable'
 
 class Person < Nameable
-  def initialize(age, name = 'Unknown', parent_permission: true)
+  def initialize(age, name = 'Unknown', parent_permission: true, rentals)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @rentals = []
     super()
   end
 
